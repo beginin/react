c21606cf4d8f3a416d1a9e41c3f4eda3b2469273
Vladimir <beginin@mail.ru>
2013-08-28 11:59:06 +0200

    Add ...

--- app/models/worksheet.rb
+++ app/models/worksheet.rb
@@ -1,2 +1,5 @@
 class Worksheet < ActiveRecord::Base
+  attr_accessor :sex, :age, :language, :specialty, :city
+  validates :sex, :age, :language, :specialty, :city, presence: 	true
+  validates :age, numericality: { only_integer: true , :greater_than => 0, :less_than_or_equal_to => 100}
 end
